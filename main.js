85336ee0e258d0d35e31c8a670eaf5a605580fc6
minhkhoa2404 <minhkhoatranhoang244@gmail.com>
2023-07-29 17:17:39 +0700

    config manifest, main, html

--- main.js
+++ main.js
@@ -1,362 +1,376 @@
-const a = {
-  "Introduction to Kotlin": [
-    "question-0-2",
-    "question-1-1",
-    "question-2-0",
-    "question-2-2",
-    "question-2-3",
-    "question-3-0",
-    "question-4-1",
-    "question-5-0",
-    "question-6-2",
-    "question-7-0",
-    "question-7-1",
-    "question-7-3",
-    "question-8-0",
-    "question-8-2",
-    "question-8-3",
-    "question-9-0",
-  ],
-  "Setup Android Studio": [
-    "question-0-0",
-    "question-1-0",
-    "question-1-1",
-    "question-1-2",
-    "question-2-2",
-    "question-3-1",
-    "question-3-2",
-    "question-3-4",
-    "question-4-4",
-    "question-5-3",
-    "question-6-0",
-    "question-7-2",
-    "question-8-3",
-    "question-9-2",
-  ],
-  "Build a basic layout": [
-    "question-0-0",
-    "question-1-0",
-    "question-2-2",
-    "question-3-1",
-    "question-3-3",
-    "question-4-1",
-    "question-5-2",
-    "question-6-1",
-    "question-7-3",
-    "question-8-0",
-    "question-9-2",
-  ],
-  "Kotlin fundamentals": [
-    "question-0-1",
-    "question-1-0",
-    "question-1-1",
-    "question-1-2",
-    "question-2-1",
-    "question-3-1",
-    "question-4-1",
-    "question-5-2",
-    "question-6-1",
-    "question-7-3",
-    "question-8-0",
-    "question-9-1",
-  ],
-  "Add a button to an app": [
-    "question-0-2",
-    "question-1-0",
-    "question-2-0",
-    "question-3-0",
-    "question-4-3",
-    "question-5-2",
-    "question-6-1",
-  ],
-  "Interacting with UI and state": [
-    "question-0-0",
-    "question-1-0",
-    "question-2-1",
-    "question-3-0",
-    "question-4-1",
-    "question-5-1",
-    "question-6-1",
-    "question-7-0",
-    "question-8-1",
-    "question-9-2",
-  ],
-  "More Kotlin fundamentals": [
-    "question-0-0",
-    "question-1-0",
-    "question-2-3",
-    "question-3-0",
-    "question-4-3",
-    "question-5-0",
-    "question-5-2",
-    "question-5-3",
-    "question-6-0",
-    "question-6-1",
-    "question-7-1",
-    "question-8-0",
-    "question-9-3",
-  ],
-  "Build a scrollable list": [
-    "question-0-0",
-    "question-1-2",
-    "question-2-3",
-    "question-3-1",
-    "question-4-0",
-    "question-5-2",
-    "question-6-3",
-    "question-7-0",
-  ],
-  "Build beautiful apps": [
-    "question-0-3",
-    "question-1-1",
-    "question-2-0",
-    "question-3-0",
-    "question-4-2",
-    "question-5-1",
-    "question-6-0",
-    "question-7-0",
-    "question-8-0",
-    "question-9-3",
-    "question-10-1",
-    "question-11-2",
-  ],
-  "Architecture Components": [
-    "question-0-0",
-    "question-1-3",
-    "question-2-1",
-    "question-3-1",
-    "question-4-0",
-    "question-5-0",
-    "question-6-1",
-    "question-7-0",
-    "question-8-1",
-    "question-9-1",
-  ],
-  "Navigation in Jetpack Compose": [
-    "question-0-2",
-    "question-1-0",
-    "question-2-0",
-    "question-3-2",
-    "question-4-1",
-    "question-5-3",
-    "question-6-1",
-    "question-7-1",
-    "question-8-1",
-    "question-9-0",
-    "question-10-0",
-    "question-10-5",
-  ],
-  "Adapt for different screen sizes": [
-    "question-0-1",
-    "question-1-0",
-    "question-1-2",
-    "question-1-3",
-    "question-2-3",
-    "question-3-0",
-    "question-4-3",
-    "question-5-2",
-    "question-6-1",
-    "question-7-0",
-    "question-8-0",
-    "question-9-3",
-  ],
-  "Get data from the internet": [
-    "question-0-0",
-    "question-2-1",
-    "question-2-2",
-    "question-2-3",
-    "question-3-0",
-    "question-4-0",
-    "question-4-3",
-    "question-5-0",
-    "question-5-1",
-    "question-6-2",
-    "question-7-0",
-    "question-8-2",
-    "question-9-3",
-  ],
-  "Load and display images from the internet": [
-    "question-0-3",
-    "question-1-0",
-    "question-2-2",
-    "question-3-3",
-    "question-4-1",
-    "question-5-2",
-    "question-6-0",
-    "question-6-1",
-    "question-6-2",
-    "question-7-1",
-    "question-8-3",
-    "question-9-0",
-  ],
-  "Introduction to SQL": [
-    "question-0-0",
-    "question-0-3",
-    "question-1-1",
-    "question-2-0",
-    "question-3-2",
-    "question-4-1",
-    "question-5-1",
-    "question-5-3",
-    "question-6-1",
-    "question-7-2",
-    "question-8-1",
-    "question-9-0",
-  ],
-  "Use Room for data persistence": [
-    "question-0-3",
-    "question-1-3",
-    "question-2-2",
-    "question-3-1",
-    "question-4-1",
-    "question-4-2",
-    "question-4-3",
-    "question-5-0",
-    "question-6-0",
-    "question-6-2",
-    "question-6-3",
-    "question-7-0",
-    "question-8-2",
-  ],
-  "Store and access data using keys with DataStore": [
-    "question-0-0",
-    "question-0-1",
-    "question-1-1",
-    "question-2-3",
-    "question-3-0",
-    "question-4-1",
-  ],
-  "Schedule tasks with WorkManager": [
-    "question-0-1",
-    "question-1-0",
-    "question-1-2",
-    "question-1-3",
-    "question-2-0",
-    "question-2-3",
-    "question-3-1",
-    "question-4-2",
-    "question-5-1",
-    "question-6-0",
-    "question-6-1",
-    "question-6-2",
-    "question-7-1",
-    "question-8-2",
-    "question-9-0",
-  ],
-  "Android Views and Compose in Views": [
-    "question-0-2",
-    "question-1-0",
-    "question-2-0",
-    "question-3-1",
-    "question-4-1",
-    "question-5-1",
-    "question-6-0",
-    "question-7-1",
-    "question-8-0",
-    "question-9-1",
-  ],
-  "Views in Compose": [
-    "question-0-3",
-    "question-1-0",
-    "question-2-0",
-    "question-2-2",
-    "question-2-3",
-    "question-3-0",
-    "question-4-2",
-  ],
-};
+document.getElementById('start').addEventListener('click', () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const tab = tabs[0];
 
-const url = document.URL;
+      function mainFunc() {
+        const a = {
+          "Introduction to Kotlin": [
+            "question-0-2",
+            "question-1-1",
+            "question-2-0",
+            "question-2-2",
+            "question-2-3",
+            "question-3-0",
+            "question-4-1",
+            "question-5-0",
+            "question-6-2",
+            "question-7-0",
+            "question-7-1",
+            "question-7-3",
+            "question-8-0",
+            "question-8-2",
+            "question-8-3",
+            "question-9-0",
+          ],
+          "Setup Android Studio": [
+            "question-0-0",
+            "question-1-0",
+            "question-1-1",
+            "question-1-2",
+            "question-2-2",
+            "question-3-1",
+            "question-3-2",
+            "question-3-4",
+            "question-4-4",
+            "question-5-3",
+            "question-6-0",
+            "question-7-2",
+            "question-8-3",
+            "question-9-2",
+          ],
+          "Build a basic layout": [
+            "question-0-0",
+            "question-1-0",
+            "question-2-2",
+            "question-3-1",
+            "question-3-3",
+            "question-4-1",
+            "question-5-2",
+            "question-6-1",
+            "question-7-3",
+            "question-8-0",
+            "question-9-2",
+          ],
+          "Kotlin fundamentals": [
+            "question-0-1",
+            "question-1-0",
+            "question-1-1",
+            "question-1-2",
+            "question-2-1",
+            "question-3-1",
+            "question-4-1",
+            "question-5-2",
+            "question-6-1",
+            "question-7-3",
+            "question-8-0",
+            "question-9-1",
+          ],
+          "Add a button to an app": [
+            "question-0-2",
+            "question-1-0",
+            "question-2-0",
+            "question-3-0",
+            "question-4-3",
+            "question-5-2",
+            "question-6-1",
+          ],
+          "Interacting with UI and state": [
+            "question-0-0",
+            "question-1-0",
+            "question-2-1",
+            "question-3-0",
+            "question-4-1",
+            "question-5-1",
+            "question-6-1",
+            "question-7-0",
+            "question-8-1",
+            "question-9-2",
+          ],
+          "More Kotlin fundamentals": [
+            "question-0-0",
+            "question-1-0",
+            "question-2-3",
+            "question-3-0",
+            "question-4-3",
+            "question-5-0",
+            "question-5-2",
+            "question-5-3",
+            "question-6-0",
+            "question-6-1",
+            "question-7-1",
+            "question-8-0",
+            "question-9-3",
+          ],
+          "Build a scrollable list": [
+            "question-0-0",
+            "question-1-2",
+            "question-2-3",
+            "question-3-1",
+            "question-4-0",
+            "question-5-2",
+            "question-6-3",
+            "question-7-0",
+          ],
+          "Build beautiful apps": [
+            "question-0-3",
+            "question-1-1",
+            "question-2-0",
+            "question-3-0",
+            "question-4-2",
+            "question-5-1",
+            "question-6-0",
+            "question-7-0",
+            "question-8-0",
+            "question-9-3",
+            "question-10-1",
+            "question-11-2",
+          ],
+          "Architecture Components": [
+            "question-0-0",
+            "question-1-3",
+            "question-2-1",
+            "question-3-1",
+            "question-4-0",
+            "question-5-0",
+            "question-6-1",
+            "question-7-0",
+            "question-8-1",
+            "question-9-1",
+          ],
+          "Navigation in Jetpack Compose": [
+            "question-0-2",
+            "question-1-0",
+            "question-2-0",
+            "question-3-2",
+            "question-4-1",
+            "question-5-3",
+            "question-6-1",
+            "question-7-1",
+            "question-8-1",
+            "question-9-0",
+            "question-10-0",
+            "question-10-5",
+          ],
+          "Adapt for different screen sizes": [
+            "question-0-1",
+            "question-1-0",
+            "question-1-2",
+            "question-1-3",
+            "question-2-3",
+            "question-3-0",
+            "question-4-3",
+            "question-5-2",
+            "question-6-1",
+            "question-7-0",
+            "question-8-0",
+            "question-9-3",
+          ],
+          "Get data from the internet": [
+            "question-0-0",
+            "question-2-1",
+            "question-2-2",
+            "question-2-3",
+            "question-3-0",
+            "question-4-0",
+            "question-4-3",
+            "question-5-0",
+            "question-5-1",
+            "question-6-2",
+            "question-7-0",
+            "question-8-2",
+            "question-9-3",
+          ],
+          "Load and display images from the internet": [
+            "question-0-3",
+            "question-1-0",
+            "question-2-2",
+            "question-3-3",
+            "question-4-1",
+            "question-5-2",
+            "question-6-0",
+            "question-6-1",
+            "question-6-2",
+            "question-7-1",
+            "question-8-3",
+            "question-9-0",
+          ],
+          "Introduction to SQL": [
+            "question-0-0",
+            "question-0-3",
+            "question-1-1",
+            "question-2-0",
+            "question-3-2",
+            "question-4-1",
+            "question-5-1",
+            "question-5-3",
+            "question-6-1",
+            "question-7-2",
+            "question-8-1",
+            "question-9-0",
+          ],
+          "Use Room for data persistence": [
+            "question-0-3",
+            "question-1-3",
+            "question-2-2",
+            "question-3-1",
+            "question-4-1",
+            "question-4-2",
+            "question-4-3",
+            "question-5-0",
+            "question-6-0",
+            "question-6-2",
+            "question-6-3",
+            "question-7-0",
+            "question-8-2",
+          ],
+          "Store and access data using keys with DataStore": [
+            "question-0-0",
+            "question-0-1",
+            "question-1-1",
+            "question-2-3",
+            "question-3-0",
+            "question-4-1",
+          ],
+          "Schedule tasks with WorkManager": [
+            "question-0-1",
+            "question-1-0",
+            "question-1-2",
+            "question-1-3",
+            "question-2-0",
+            "question-2-3",
+            "question-3-1",
+            "question-4-2",
+            "question-5-1",
+            "question-6-0",
+            "question-6-1",
+            "question-6-2",
+            "question-7-1",
+            "question-8-2",
+            "question-9-0",
+          ],
+          "Android Views and Compose in Views": [
+            "question-0-2",
+            "question-1-0",
+            "question-2-0",
+            "question-3-1",
+            "question-4-1",
+            "question-5-1",
+            "question-6-0",
+            "question-7-1",
+            "question-8-0",
+            "question-9-1",
+          ],
+          "Views in Compose": [
+            "question-0-3",
+            "question-1-0",
+            "question-2-0",
+            "question-2-2",
+            "question-2-3",
+            "question-3-0",
+            "question-4-2",
+          ],
+        };
+        
+        function getTitle(url) {
+          if (url.includes("android-basics-compose-unit-1-pathway-1")) {
+            return "Introduction to Kotlin";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-1-pathway-2")) {
+            return "Setup Android Studio";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-1-pathway-3")) {
+            return "Build a basic layout";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-2-pathway-1")) {
+            return "Kotlin fundamentals";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-2-pathway-2")) {
+            return "Add a button to an app";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-2-pathway-3")) {
+            return "Interacting with UI and state";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-3-pathway-1")) {
+            return "More Kotlin fundamentals";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-3-pathway-2")) {
+            return "Build a scrollable list";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-3-pathway-3")) {
+            return "Build beautiful apps";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-4-pathway-1")) {
+            return "Architecture Components";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-4-pathway-2")) {
+            return "Navigation in Jetpack Compose";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-4-pathway-3")) {
+            return "Adapt for different screen sizes";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-5-pathway-1")) {
+            return "Get data from the internet";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-5-pathway-2")) {
+            return "Load and display images from the internet";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-6-pathway-1")) {
+            return "Introduction to SQL";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-6-pathway-2")) {
+            return "Use Room for data persistence";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-6-pathway-3")) {
+            return "Store and access data using keys with DataStore";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-7-pathway-1")) {
+            return "Schedule tasks with WorkManager";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-8-pathway-1")) {
+            return "Android Views and Compose in Views";
+          } 
+          
+          else if (url.includes("android-basics-compose-unit-8-pathway-2")) {
+            return "Views in Compose";
+          } 
+        };
 
-function getTitle(url) {
-  if (url.includes("android-basics-compose-unit-1-pathway-1")) {
-    return "Introduction to Kotlin";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-1-pathway-2")) {
-    return "Setup Android Studio";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-1-pathway-3")) {
-    return "Build a basic layout";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-2-pathway-1")) {
-    return "Kotlin fundamentals";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-2-pathway-2")) {
-    return "Add a button to an app";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-2-pathway-3")) {
-    return "Interacting with UI and state";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-3-pathway-1")) {
-    return "More Kotlin fundamentals";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-3-pathway-2")) {
-    return "Build a scrollable list";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-3-pathway-3")) {
-    return "Build beautiful apps";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-4-pathway-1")) {
-    return "Architecture Components";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-4-pathway-2")) {
-    return "Navigation in Jetpack Compose";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-4-pathway-3")) {
-    return "Adapt for different screen sizes";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-5-pathway-1")) {
-    return "Get data from the internet";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-5-pathway-2")) {
-    return "Load and display images from the internet";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-6-pathway-1")) {
-    return "Introduction to SQL";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-6-pathway-2")) {
-    return "Use Room for data persistence";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-6-pathway-3")) {
-    return "Store and access data using keys with DataStore";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-7-pathway-1")) {
-    return "Schedule tasks with WorkManager";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-8-pathway-1")) {
-    return "Android Views and Compose in Views";
-  } 
-  
-  else if (url.includes("android-basics-compose-unit-8-pathway-2")) {
-    return "Views in Compose";
-  } 
-}
+        const url = document.URL;
+        
+        
+        const x = document.getElementsByClassName("devsite-quiz-body");
+        const title = getTitle(url);
+        
+        a[title].forEach((a) => {
+          document.getElementById(a).click();
+        });
+        
+        x[0].children[1].click()
+      };
 
-const x = document.getElementsByClassName("devsite-quiz-body");
-const title = getTitle(url);
-
-a[title].forEach((a) => {
-  document.getElementById(a).click();
+      
+      chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          func: mainFunc,
+          //        files: ['contentScript.js'],  // To call external file instead
+      }).then(() => console.log('Injected a function!'));
+  });
 });
-
-x[0].children[1].click()
-
-//devsite-quiz-submit-button
